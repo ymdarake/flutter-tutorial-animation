30b14ac8b25674e287636a76d898a449a3390c75
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-24 19:15:27 +1300

    init

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        // created again and again (cheap)
+        return Container(
+          child: child, //reused
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(), // created only once (expensive)
+    );
+  }
+}
